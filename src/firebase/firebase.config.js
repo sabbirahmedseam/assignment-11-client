814a8c1c13fe916bfd4ccc66bab26d2375e3a8b3
sabbirahmedseam <sabbirahmedseam1@gmail.com>
2022-11-07 18:57:56 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBRQVahPlBqgw1tkfzI8hzA37iIGBsK-gk",
+  authDomain: "mirpur-photography.firebaseapp.com",
+  projectId: "mirpur-photography",
+  storageBucket: "mirpur-photography.appspot.com",
+  messagingSenderId: "310764252383",
+  appId: "1:310764252383:web:aa158bea6efeac375b8d6a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
+
+// mirpur-photography
