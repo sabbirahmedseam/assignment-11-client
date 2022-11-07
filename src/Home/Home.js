794fb9db96951927f2fbd378c0b85ad1abb75286
sabbirahmedseam <sabbirahmedseam1@gmail.com>
2022-11-07 19:13:29 +0600

    routes

---
+++ src/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div className='div'>
+                Home
+                HOUSeee
+        </div>
+    );
+};
+
+export default Home;
