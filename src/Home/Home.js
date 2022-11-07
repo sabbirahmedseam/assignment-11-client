7eb80f2f18de350764b69beef9db6aa1a26e90db
sabbirahmedseam <sabbirahmedseam1@gmail.com>
2022-11-07 19:26:30 +0600

    send

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import './Home.css'
+import React from "react";
+import "./Home.css";
 
 const Home = () => {
-    return (
-        <div className='div'>
-                Home
-                HOUSeee
-        </div>
-    );
+  return (
+    <div className="div">
+      <h1> Home HOUSeee</h1>
+    </div>
+  );
 };
 
 export default Home;
